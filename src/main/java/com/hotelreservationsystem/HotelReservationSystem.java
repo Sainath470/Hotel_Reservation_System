5fea540583ff53f1115712b6219f25565ee6a44a
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-15 22:48:07 +0530

    ADDED : Ability to add hotel name and rates for regular customers

--- src/main/java/com/hotelreservationsystem/HotelReservationSystem.java
+++ src/main/java/com/hotelreservationsystem/HotelReservationSystem.java
@@ -1,6 +1,16 @@
 package com.hotelreservationsystem;
-public class HotelReservationSystem {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Hotel Reservation Program");
+public class HotelReservationSystem{
+    String hotelName;
+    int weekdayRateForRegularCustomer;
+    int weekendRateForRegularCustomer;
+
+
+    public  HotelReservationSystem(String name, int weekdayRegular, int weekendRegular){
+        this.hotelName = name;
+        this.weekdayRateForRegularCustomer = weekdayRegular;
+        this.weekendRateForRegularCustomer = weekendRegular;
+        System.out.println("HOTEL NAME :"+hotelName+"\nWEEKDAY RATE FOR REGULAR CUSTOMER:"+weekdayRateForRegularCustomer+"\nWEEKEND RATE FOR REGULAR CUSTOMER:"+ weekendRateForRegularCustomer+"\n");
     }
+
+
 }

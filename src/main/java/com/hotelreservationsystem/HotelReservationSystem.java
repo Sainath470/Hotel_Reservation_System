47efadfb0df0649f067c9e75d2b8b1b7699ae583
Akula Venkata Sainath <78947251+Sainath470@users.noreply.github.com>
2021-04-15 09:33:47 +0530

    ADDED : Welcome message to the hotel reservation program

---
+++ src/main/java/com/hotelreservationsystem/HotelReservationSystem.java
@@ -0,0 +1,6 @@
+package com.hotelreservationsystem;
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hotel Reservation Program");
+    }
+}
